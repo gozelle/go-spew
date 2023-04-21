e2d2a75805d1996ffd354e04b0ee04bbe8481805
koyeo <guyue.me@gmail.com>
2023-04-22 00:30:54 +0800

    add print caller

---
+++ debug.go
@@ -0,0 +1,14 @@
+package spew
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func runFuncPos() string {
+	pc := make([]uintptr, 1)
+	runtime.Callers(2, pc)
+	f := runtime.FuncForPC(pc[0])
+	file, line := f.FileLine(f.Entry())
+	return fmt.Sprintf("%s:%d", file, line)
+}
---
+++ json_test.go
@@ -0,0 +1,7 @@
+package spew
+
+import "testing"
+
+func TestJson(t *testing.T) {
+	Json("a")
+}
